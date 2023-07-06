e90f2bc6719158327a092caca8de79d33c82e1e8
Arajakriti04 <113169544+Arajakriti04@users.noreply.github.com>
2023-07-06 18:18:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let celsiusInput = document.getElementById('celcius');
+let fahrenheitInput = document.getElementById('fahrenheit')
+let kelvinInput = document.getElementById('kelvin')
+let btn = document.querySelector('.button button')
+
+function roundNumber(number){
+    return Math.round(number*100)/100
+}
+
+
+/* Celcius to Fahrenheit and Kelvin */
+celsiusInput.addEventListener('input', function(){
+    let cTemp = parseFloat(celsiusInput.value)
+    let fTemp = (cTemp*(9/5)) + 32
+    let kTemp = cTemp + 273.15
+
+    fahrenheitInput.value = roundNumber(fTemp)
+    kelvinInput.value = roundNumber(kTemp)
+})
+
+
+/* Fahrenheit to Celcius and Kelvin */
+fahrenheitInput.addEventListener('input', function(){
+    let fTemp = parseFloat(fahrenheitInput.value)
+    let cTemp = (fTemp - 32) * (5/9)
+    let kTemp = (fTemp -32) * (5/9) + 273.15
+
+    celsiusInput.value = roundNumber(cTemp)
+    kelvinInput.value = roundNumber(kTemp)
+})
+
+/* Kelvin to Celcius and Fahrenheit */
+kelvinInput.addEventListener('input', function(){
+    let kTemp = parseFloat(kelvinInput.value)
+    let cTemp = kTemp - 273.15
+    let fTemp = (kTemp - 273.15) * (9/5) + 32
+
+    celsiusInput.value = roundNumber(cTemp)
+    fahrenheitInput.value = roundNumber(fTemp)
+})
+
+
+// btn.addEventListener('click',()=>{
+//     celsiusInput.value=""
+//     fahrenheitInput.value =""
+//     kelvinInput.value =""
+
+// })
+function clicked(){
+    celsiusInput.value=""
+        fahrenheitInput.value =""
+        kelvinInput.value =""
+}
